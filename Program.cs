15f2d827e7e3db307b5735423be363c3cdb0071f
jorlmn <114875835+jorlmn@users.noreply.github.com>
2022-12-22 15:57:10 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Teste com Nokia
+Smartphone nokiaCell = new Nokia(numero: "123456", modelo: "nokiaA1", imei: "11111111", memoria: 64);
+Console.WriteLine($"{nokiaCell.Modelo} {nokiaCell.Numero} {nokiaCell.IMEI} {nokiaCell.Memoria}");
+nokiaCell.Ligar();
+nokiaCell.InstalarAplicativo("Spotify");
+
+Console.WriteLine("");
+
+// Teste com Iphone
+Smartphone iphoneCell = new Iphone(numero: "654321", modelo: "iphoneA1", imei: "222222", memoria: 64);
+Console.WriteLine($"{iphoneCell.Modelo} {iphoneCell.Numero} {iphoneCell.IMEI} {iphoneCell.Memoria}");
+iphoneCell.ReceberLigacao();
+iphoneCell.InstalarAplicativo("Youtube");
+
+
